120e5702ca5b48baf7ab89af5949ad93356bbab3
Moonpreneur <harsh.r@moonpreneur.com>
2024-07-06 11:03:13 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,39 @@
+const questions = [
+    { targetOunces: '24 ounces'},
+    { targetOunces: '32 ounces'},
+    { targetOunces: '40 ounces'},
+    { targetOunces: '48 ounces'},
+    { targetOunces: '56 ounces'},
+    { targetOunces: '64 ounces'},
+    { targetOunces: '72 ounces'},
+    { targetOunces: '80 ounces'},
+    { targetOunces: '88 ounces'},
+    { targetOunces: '96 ounces'},
+    { targetOunces: '104 ounces'},
+    { targetOunces: '112 ounces'},
+    { targetOunces: '120 ounces'},
+    { targetOunces: '128 ounces'},
+    { targetOunces: '136 ounces'},
+    { targetOunces: '144 ounces'}
+];
+
+const values = [1, 2, 3, 4, 5, 6];
+
+/*Number of solutions for each value:
+24 ounces : 1
+32 ounces : 3
+40 ounces : 6
+48 ounces : 10
+56 ounces : 15
+64 ounces : 21
+72 ounces : 25
+80 ounces : 27
+88 ounces : 27
+96 ounces : 25
+104 ounces: 21
+112 ounces : 15
+120 ounces : 10
+128 ounces : 6
+136 ounces : 3
+144 ounces : 1
+*/
